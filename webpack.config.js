f41a8baa74e197f9de48a6d241f40848a0071127
Pavel Silber <34262772+silberpavel@users.noreply.github.com>
2018-10-29 14:08:18 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+let webpack = require('webpack');
+let HtmlPlugin = require('html-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let loaders = require('./webpack.config.loaders')();
+let path = require('path');
+
+loaders.push({
+    test: /\.css$/,
+    loader: ExtractTextPlugin.extract({
+        fallbackLoader: 'style-loader',
+        loader: 'css-loader'
+    })
+});
+
+module.exports = {
+    entry: {
+        main: './src/index.js',
+        dnd: './src/dnd.js'
+    },
+    output: {
+        filename: '[hash].js',
+        path: path.resolve('dist')
+    },
+    devtool: 'source-map',
+    module: {
+        loaders
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                drop_debugger: false
+            }
+        }),
+        new ExtractTextPlugin('styles.css'),
+        new HtmlPlugin({
+            title: 'Main Homework',
+            template: 'index.hbs',
+            chunks: ['main']
+        }),
+        new HtmlPlugin({
+            title: 'Div Drag And Drop',
+            template: 'dnd.hbs',
+            filename: 'dnd.html',
+            chunks: ['dnd']
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
+};
